f78f6332172c31ccc5737c95a9b0fe382aef8f13
Michał Lach <michal.lach@misys.com>
2014-09-23 10:57:29 +0200

    Basic application structure

---
+++ Gruntfile.js
@@ -0,0 +1,265 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+  // Load grunt tasks automatically
+  require('load-grunt-tasks')(grunt);
+
+  // Time how long tasks take. Can help when optimizing build times
+  require('time-grunt')(grunt);
+
+  // Define the configuration for all the tasks
+  grunt.initConfig({
+    ngdocs: {
+      options: {
+        dest: 'docs',
+        html5Mode: false,
+        startPage: '/controllers',
+        scripts: ['js/build/libs.js',
+          'js/build/controllers.js',
+          'js/build/directives.js',
+          'js/build/filters.js',
+          'js/build/modules.js',
+          'js/build/services.js'
+        ],
+        title: 'Diplomacy Manager Documentation'
+      },
+      controllers: {
+        src: ['js/src/controllers/*.js', 'js/src/controllers/**/*.js', 'js/src/controllers/**/**/*.js'],
+        title: 'Controllers'
+      },
+      directives: {
+        src: ['js/src/directives/*.js', 'js/src/directives/**/*.js', 'js/src/directives/**/**/*.js'],
+        title: 'Directives'
+      },
+      services: {
+        src: ['js/src/services/*.js', 'js/src/services/**/*.js', 'js/src/services/**/**/*.js'],
+        title: 'Services'
+      },
+    },
+
+    karma: {
+      unit: {
+        configFile: 'karma.conf.js',
+      },
+      e2e: {
+        configFile: 'karma-e2e.conf.js',
+      }
+    },
+
+    bower: {
+      install: {
+        targetDir: 'libs/',
+        cleanBowerDir: true,
+        cleanup: true
+      }
+    },
+
+
+    sass: {
+      dist: {
+        options: {
+          style: 'expanded'
+        },
+        files: {
+          'styles/css/styles.css': 'styles/sass/styles.scss'
+        }
+      }
+    },
+
+    concat: {
+      options: {
+        separator: ';',
+      },
+      libs: {
+        src: [
+          'libs/angular/angular.js',
+          'libs/angular-animate/angular-animate.js',
+          'libs/angular-cookies/angular-cookies.js',
+          'libs/angular-loader/angular-loader.js',
+          'libs/angular-mocks/angular-mocks.js',
+          'libs/angular-route/angular-route.js',
+          'libs/angular-sanitize/angular-sanitize.js',
+          'libs/angular-scenario/angular-scenario.js'
+        ],
+        dest: 'js/build/libs.js'
+      },
+      directives: {
+        src: ['js/src/directives/**/*.js'],
+        dest: 'js/build/directives.js'
+      },
+      controllers: {
+        src: ['js/src/controllers/**/*.js'],
+        dest: 'js/build/controllers.js'
+      },
+      services: {
+        src: ['js/src/services/**/*.js'],
+        dest: 'js/build/services.js'
+      },
+      filters: {
+        src: ['js/src/filters/**/*.js'],
+        dest: 'js/build/filters.js'
+      }
+    },
+
+    uglify: {
+      options: {
+        mangle: false
+      },
+      build: {
+        files: {
+          'js/build/app.min.js': [
+            'js/src/app.js',
+            'js/build/controllers.js',
+            'js/build/directives.js',
+            'js/build/filters.js',
+            'js/build/modules.js',
+            'js/build/services.js'
+          ],
+          'js/build/libs.min.js': [
+            'js/build/libs.js'
+          ]
+        }
+      }
+    },
+
+    scriptlinker: {
+      devlib: {
+        options: {
+          startTag: '<!--LIBS-->',
+          endTag: '<!--LIBS END-->',
+          fileTmpl: '<script src="/%s"></script>'
+        },
+        files: {
+          'index.jsp': [
+            'js/build/libs.js'
+          ]
+        }
+      },
+      devapp: {
+        options: {
+          startTag: '<!--SCRIPTS-->',
+          endTag: '<!--SCRIPTS END-->',
+          fileTmpl: '<script src="/%s"></script>'
+        },
+        files: {
+          'index.jsp': [
+            'js/src/app.js',
+            'js/build/controllers.js',
+            'js/build/filters.js',
+            'js/build/services.js',
+            'js/build/directives.js',
+            'js/build/modules.js'
+          ]
+        }
+      },
+    },
+
+    /**/
+
+    watch: {
+      styles: {
+        files: ['styles/sass/*.scss'],
+        tasks: ['sass'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      directives: {
+        files: ['js/src/directives/**/*.js'],
+        tasks: ['concat:directives'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      services: {
+        files: ['js/src/services/*.js'],
+        tasks: ['concat:services'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      filters: {
+        files: ['js/src/filters/**/*.js'],
+        tasks: ['concat:filters'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      controllers: {
+        files: ['js/src/controllers/**/*.js'],
+        tasks: ['concat:controllers'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      app: {
+        files: ['js/src/*.js'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+      html: {
+        files: ['views/*.html', 'views/**/*.html'],
+        options: {
+          livereload: {
+            port: 9001
+          }
+        },
+      },
+    },
+
+    connect: {
+      server: {
+        options: {
+          port: 1337,
+          hostname: '*',
+          livereload: 9001,
+          open: true,
+        }
+      },
+    },
+
+  });
+
+  grunt.registerTask('documentation', ['ngdocs']);
+
+
+  grunt.registerTask('default', function() {
+    grunt.task.run([
+
+    ]);
+  });
+
+  grunt.registerTask('production', function() {
+    grunt.task.run([
+      'bower:install',
+      'concat',
+      'less',
+      'uglify',
+
+    ]);
+  });
+
+  grunt.registerTask('dev', function() {
+    grunt.task.run([
+      'bower:install',
+      'sass',
+      'connect',
+      'watch'
+    ]);
+  });
+
+};
--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 Diplomacy-manager
 =================
+
+The aim of the sample application is to build a simple “diplomacy manager”, a tool that
+Orcs and Barbarians alliance members could use to manage which player is talking to
+which alliance.
