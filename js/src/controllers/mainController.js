594ba910b0053f3a63c3f844601c82b748dcec90
Michał Lach <kontakt@michal-lach.pl>
2014-09-25 15:23:44 +0200

    Login view and user view

--- js/src/controllers/mainController.js
+++ js/src/controllers/mainController.js
@@ -1,5 +1,10 @@
 'use strict';
 
-diplomacyManager.controller('mainController', ['$scope',
-  function($scope) {}
+diplomacyManager.controller('mainController', ['$scope', '$location',
+  function($scope, $location) {
+
+    $scope.signIn = function(username) {
+      $location.path('/users');
+    };
+  }
 ]);
